cfbf3ed1ff922a2d58fded46086e615e33576c15
fafa <3107393960@qq.com>
2024-12-19 15:24:44 +0800

    翆璃塔模组v0.0.1e_Beta_Patch版本更新一览: 1、测试物品“格林卡收集器”将通过随机数增加收集度 2、本次更新为Patch更新(补丁更新) 2024.12.19_15:24

--- src/main/java/com/curiter/item/custom/GARUINKA_COLLECTORS.java
+++ src/main/java/com/curiter/item/custom/GARUINKA_COLLECTORS.java
@@ -2,12 +2,14 @@ package com.curiter.item.custom;
 
 import com.curiter.block.ModBlocks;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 public class GARUINKA_COLLECTORS extends Item {
@@ -26,11 +28,19 @@ public class GARUINKA_COLLECTORS extends Item {
             BlockState BlockState = world.getBlockState(context.getBlockPos());
             if (BlockIsRight(BlockState)){
                 ObtainGaruinka(player,context.getWorld(),context.getBlockPos());
-                context.getStack().damage(-2500,context.getPlayer(),PlayerEntity -> PlayerEntity.sendToolBreakStatus(PlayerEntity.getActiveHand()));
+                context.getStack().damage(-500-Rollumber(),context.getPlayer(),PlayerEntity -> PlayerEntity.sendToolBreakStatus(PlayerEntity.getActiveHand()));
             }
         }
 
         return super.useOnBlock(context);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient()){
+
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
     }
 
     @Override
@@ -45,4 +55,9 @@ public class GARUINKA_COLLECTORS extends Item {
     private void ObtainGaruinka(PlayerEntity player,World world,BlockPos blockPos){
         world.breakBlock(blockPos,false);
     }
+
+    private static int Rollumber(){
+        return Random.createLocal().nextInt(2501);
+    }
+
 }
