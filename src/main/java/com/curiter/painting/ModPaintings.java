a91789e9ab182830c67405ed230180d9a0e1ae89
fafa <3107393960@qq.com>
2024-12-30 23:59:53 +0800

    翆璃塔模组v0.0.2a_Beta_Patch1版本更新一览: 1、添加了以下画作：“翆璃塔(消逝虚无的历史)(图片所属：14)”、“翆璃塔(突破焰心的新生)(作者：彩笔小狼彩笔)” 2、本次更新为Patch更新(补丁更新) 2024.12.30_23:59

--- src/main/java/com/curiter/painting/ModPaintings.java
+++ src/main/java/com/curiter/painting/ModPaintings.java
@@ -13,6 +13,8 @@ public class ModPaintings {
     public static final PaintingVariant ANCIENT_CASTLE_UNDER_THE_BLOOD_MOON_PAINTING = registerPainting("ancient_castle_under_the_blood_moon_painting",new PaintingVariant(64,48));
     public static final PaintingVariant SLEEPING_ATLANTIS_PAINTING = registerPainting("sleeping_atlantis_painting",new PaintingVariant(64,32));
     public static final PaintingVariant HECHUWU1 = registerPainting("hechuwu1",new PaintingVariant(16,16));
+    public static final PaintingVariant CURITER = registerPainting("curiter",new PaintingVariant(16,16));
+    public static final PaintingVariant CURITER_NEW = registerPainting("curiter_new",new PaintingVariant(16,16));
 
     private static PaintingVariant registerPainting(String name,PaintingVariant paintingVariant){
         return Registry.register(Registries.PAINTING_VARIANT,new Identifier(CuriterMod.MOD_ID,name),paintingVariant);
